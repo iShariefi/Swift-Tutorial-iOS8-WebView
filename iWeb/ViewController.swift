49c18d329b127ef10a81196c08b1b9109eed406c
iShariefi <dev@infernalsoft.company>
2015-05-23 05:36:13 +0600

    1st Commit - App Ready

--- iWeb/ViewController.swift
+++ iWeb/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var iWeb: UIWebView!
 
+    let url = "http://blog.infernalgames.co.uk"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let requestURL = NSURL(string:url)
+        let request = NSURLRequest(URL: requestURL!)
+        
+        iWeb.loadRequest(request)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
