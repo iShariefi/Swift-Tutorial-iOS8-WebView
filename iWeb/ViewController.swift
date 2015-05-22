97ff17a74542a1c933a7cecdfd36e10f77f12861
iShariefi <dev@infernalsoft.company>
2015-05-23 04:37:01 +0600

    Initial Commit

---
+++ iWeb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iWeb
+//
+//  Created by Syed Shariefi on 23/05/2015.
+//  Copyright (c) 2015 Syed Shariefi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
